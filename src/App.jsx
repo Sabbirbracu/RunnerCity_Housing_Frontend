54e7983b1b7d9dd4de705c9ec95eafa5ba90a364
SabbirAhmad <sabbir.ahmad1@g.bracu.ac.bd>
2025-10-05 16:29:19 +0600

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { Analytics } from '@vercel/analytics/react';
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { RootRouter } from "./rootRouter";
+
 
 const App = () => {
   return (
     <Provider store={store}>
       <Toaster position="top-right" reverseOrder={false} />
       <RootRouter />
+      <Analytics />
     </Provider>
   );
 };
